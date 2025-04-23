662b8d2c2a0b8d99fd71dbfc3d33b3e4ae193177
Shawnade <deshawnagalloway@yahoo.com>
2025-04-23 10:02:40 -0400

    completed 6 of 10 exercises

--- differences.py
+++ differences.py
@@ -82,3 +82,83 @@
 #   return total;
 # };
 
+# def multiply_array(ary):
+#     if len(ary) == 0:
+#         return 1
+#     total = 1
+#     for numb in ary:
+#         total *= numb
+#     return total
+
+# EXERCISE 5:
+# Fizz Buzzer
+# Write a method that takes a number argument and returns “fizz” if the number is divisible by three, “buzz” if the number is divisible by five, and “fizzbuzz” if it’s divisible by both. Here’s the javascript:
+
+# const fizzbuzzer = (x) => {
+#   if( x%(3*5) == 0 ) {
+#     return 'fizzbuzz'
+#   } else if( x%3 == 0 ) {
+#     return 'fizz'
+#   } else if ( x%5 == 0 ) {
+#     return 'buzz'
+#   } else {
+#     return 'archer'
+#   }
+# }
+
+# def fizzbuzzer(x):
+#     if x % (3 * 5) == 0:
+#         return 'fizzbuzz'
+#     elif x % 3 == 0:
+#         return 'fizz'
+#     elif x % 5 == 0:
+#         return 'buzz'
+#     else:
+#         return 'archer'
+# result = fizzbuzzer(22)
+# print(result)
+
+# EXERCISE 6:
+# Nth Fibonacci
+# Write a method that finds the fibonacci number at the nth position and returns it. Here is the javascript:
+
+# const nthFibonacciNumber = () => {
+#   let fibs = [1, 1];
+#   let num = prompt("which fibonacci number do you want?");
+
+#   while (fibs.length < parseInt(num)) {
+#     let length = fibs.length;
+#     let nextFib = fibs[length - 2] + fibs[length - 1];
+#     fibs.push(nextFib);
+#   }
+
+#   alert(fibs[fibs.length - 1] + " is the fibonacci number at position " + num);
+# };
+
+# def nth_fibonacci_number():
+#     fibs = [1, 1]
+#     num = input('which fibonacci number do you want?')
+#     num = int(num)
+#     while len(fibs) < num:
+#         length = len(fibs)
+#         next_fib = fibs[length - 2] + fibs[length - 1]
+#         fibs.append(next_fib)
+#     print(f'{fibs[-1]} is the Fibonacci number at position {num}')
+# nth_fibonacci_number()
+
+# EXERCISE 7:
+# Search Array/List
+# Write a method that searches through an array/list for a value and returns true or false depending on whether or not the value is present in the array/list. Here is the javascript:
+
+# const searchArray = (array, value) => {
+#   for(let i = 0; i < array.length-1; i++) {
+#     if(array[i] == value) {
+#       return true;
+#       break;
+#     }
+#   }
+#   return -1;
+# };
+
+def search_array(array, value):
+    
